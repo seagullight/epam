49b92c150b07bb590225c03f34543730fd418934
Andrii Chaika <seagullight@gmail.com>
2016-07-27 16:03:45 +0300

    finish

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 	
+	$("li.dropdown").mouseover(function(){
 
-
+		$(this).find(".dropdown-menu").css({"display":"block"}); 
+		$(this).siblings().find(".dropdown-menu").css({"display":"none"});
+	});
+		
+		$(".dropdown-menu").mouseout(function(){
+			$(this).css({"display":"none"});
+		
+		});
 		   });
